80e240a668b8b66845f65b9d317c36f60c996afc
Chris Perez <chrisnaperez@gmail.com>
2023-03-18 16:44:41 -0700

    mid refactor with player buttons

--- script.js
+++ script.js
@@ -2,98 +2,103 @@
 
 const body = document.body;
 
-
 let singlePlayerButton = document.querySelector("#single-player-btn");
 let twoPlayerButton = document.querySelector("#two-players-btn");
-const playerSelectButton = document.querySelector("#select-players")
+// const playerSelectButton = document.querySelector("#select-players")
 
+// div dom manipulators
 const playerInputDom = document.querySelector("#player-input-dom")
 const playerDash = document.querySelector("#player-dash-dom")
 
-const hiddenToggle = document.querySelector("#hidden");
-
+const gameDash = document.querySelector("game-dashboard");
 
 let PShiddenToggle = document.querySelector("#select-players")
-// hiddenToggle.style.display = "none";
-// PShiddenToggle.style.display = "none";
-let playerInput = document.querySelector("#player-one-name");
+let playerOneInput = document.querySelector("#player-one-name");
+let playerTwoInput = document.querySelector("#player-two-name");
 let subButton = document.querySelector("#name-sub");
 let div = document.getElementsByClassName("#player-input")
 
 
+let startOverButton = document.getElementById('reset-game-btn');
+
+
+let hidePlayerOneBtns = document.getElementById("single-player-btn")
+let hidePlayerTwoBtns = document.getElementById("two-players-btn")
 
 
 // need a game state data 
 
-const gameData = {
+let gameData = {
     playerName: [null, null],
-    players: ['', ''],
+    players: ['X', 'O'],
     board: [
         [null, null, null],
         [null, null, null],
         [null, null, null]
-    ]
+    ],
+    gameOver: false,
 }
 
 
-const selectPlayersButton = document.createElement("button"); 
-        selectPlayersButton.id ="player-one-name";
-        selectPlayersButton.textContent="Select Players"
+
 
 // player button object with event listeners -- need to refactor to forget dry
-const playerButtonObj = {
-    singlePlayer: singlePlayerButton.addEventListener('click', () => {
-        singlePlayerButton.remove();
-        twoPlayerButton.remove();
-        playerDash.append(selectPlayersButton)
-        gameData.players[0] = 'X';
-        gameData.players[1] = 'Computer';
-    }),
-    twoPlayers: twoPlayerButton.addEventListener('click', () => {
-        singlePlayerButton.remove();
-        twoPlayerButton.remove();
-        playerDash.append(selectPlayersButton)
-        gameData.players[0] = 'X';
-        gameData.players[1] = 'O';
-    }),
-    back: selectPlayersButton.addEventListener('click', () => {
-        playerDash.prepend(singlePlayerButton, twoPlayerButton);
-        selectPlayersButton.remove()
-    })
-}
+
+singlePlayerButton.addEventListener('click', () => {
+    playerDash.classList.add("hidden")
+    playerOneInput.classList.remove("hidden")
+    gameData.playerName[1] = 'Computer';
+})
+
+
+// two players 
+twoPlayerButton.addEventListener('click', () => {
+    playerDash.classList.add("hidden")
+    playerOneInput.classList.remove("hidden")
+    playerTwoInput.classList.remove("hidden")
+
+})
+
+
 
 // player name submit 
-
-const selectPlayers = () => {
-    if (gameData.players[1] === 'Computer') {
+const startGame = () => {
+    if (gameData.playerName[1] === 'Computer') {
         gameData.playerName[0] = playerInput.value;
-        console.log(gameData.playerName[0]);
-        playerDash.remove();
-        playerInputDom.remove();
-    } gameData.playerName[1] = playerInput.value;
-    PShiddenToggle.style.display = "hidden"
+        playerInputDom.classList.add("hidden")
+        playerInput.value = '';
+    } else {
+
+    }
+    // gameData.playerName[0] = playerInput.value;
+    // gameData.playerName[1] = playerInput.value;
 }
+
+
+const startOver = () => {
+    alert('Game has started over!')
+    gameData = {
+        playerName: [null, null],
+        players: ['', ''],
+        board: [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ],
+        gameOver: false,
+    }
+    playerDash.classList.remove("hidden")
+    playerOneInput.classList.add("hidden")
+    playerTwoInput.classList.add("hidden")
+    playerInputDom.classList.remove("hidden")
+}
+
+
+// helper functions 
+
 
 
 
 // event listeners  
-subButton.addEventListener('click', selectPlayers)
+startOverButton.addEventListener('click', startOver)
 
-
-// push that data itno the game state data
-
-
-// check win function
-// create win scenarios
-
-// event listener for clicks
-// store that date into the game state data
-
-// reset game button function
-// event listener 
-
-
-// 
-
-
-// 
