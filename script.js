11e7bc3d8a48733eadae24eb84bcc1435fdf8236
Chris Perez <chrisnaperez@gmail.com>
2023-03-18 13:14:16 -0700

    refactor buttons to minipulate dom

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // dom selectors
 
+const body = document.body;
+
 
 let singlePlayerButton = document.querySelector("#single-player-btn");
-const hiddenToggle = document.querySelector("#hidden");
 let twoPlayerButton = document.querySelector("#two-players-btn");
 const playerSelectButton = document.querySelector("#select-players")
+
+const playerInputDom = document.querySelector("#player-input-dom")
+const playerDash = document.querySelector("#player-dash-dom")
+
+const hiddenToggle = document.querySelector("#hidden");
+
+
 let PShiddenToggle = document.querySelector("#select-players")
-hiddenToggle.style.display = "none";
-PShiddenToggle.style.display = "none";
+// hiddenToggle.style.display = "none";
+// PShiddenToggle.style.display = "none";
 let playerInput = document.querySelector("#player-one-name");
 let subButton = document.querySelector("#name-sub");
+let div = document.getElementsByClassName("#player-input")
+
 
 
 
@@ -26,46 +36,49 @@ const gameData = {
 }
 
 
+const selectPlayersButton = document.createElement("button"); 
+        selectPlayersButton.id ="player-one-name";
+        selectPlayersButton.textContent="Select Players"
+
 // player button object with event listeners -- need to refactor to forget dry
 const playerButtonObj = {
     singlePlayer: singlePlayerButton.addEventListener('click', () => {
-        hiddenToggle.style.display = "flex";
-        singlePlayerButton = document.querySelector("#single-player-btn").disabled = true;
-        twoPlayerButton = document.querySelector("#two-players-btn").disabled = true;
-        PShiddenToggle.style.display = "flex";
+        singlePlayerButton.remove();
+        twoPlayerButton.remove();
+        playerDash.append(selectPlayersButton)
         gameData.players[0] = 'X';
         gameData.players[1] = 'Computer';
-    }), 
+    }),
     twoPlayers: twoPlayerButton.addEventListener('click', () => {
-        hiddenToggle.style.display = "flex";
-        // (hiddenToggle.style.display === "none") ? hiddenToggle.style.display = "flex" : hiddenToggle.style.display = "none"
-        twoPlayerButton = document.querySelector("#two-players-btn").disabled = true;
-        singlePlayerButton = document.querySelector("#single-player-btn").disabled = true;
-        PShiddenToggle.style.display = "flex";
+        singlePlayerButton.remove();
+        twoPlayerButton.remove();
+        playerDash.append(selectPlayersButton)
         gameData.players[0] = 'X';
         gameData.players[1] = 'O';
-    }), 
-    back: playerSelectButton.addEventListener('click', () => {
-        PShiddenToggle.style.display = "none";
-        hiddenToggle.style.display = "none";
-        singlePlayerButton = document.querySelector("#single-player-btn").disabled = false;
-        twoPlayerButton = document.querySelector("#two-players-btn").disabled = false;
+    }),
+    back: selectPlayersButton.addEventListener('click', () => {
+        playerDash.prepend(singlePlayerButton, twoPlayerButton);
+        selectPlayersButton.remove()
     })
 }
 
 // player name submit 
 
-
-// event listeners  
-subButton.addEventListener('click', () => {
+const selectPlayers = () => {
     if (gameData.players[1] === 'Computer') {
         gameData.playerName[0] = playerInput.value;
-        PShiddenToggle.style.display = "hidden"
         console.log(gameData.playerName[0]);
-    }  gameData.playerName[1] = playerInput.value; 
-    
-    
- })
+        playerDash.remove();
+        playerInputDom.remove();
+    } gameData.playerName[1] = playerInput.value;
+    PShiddenToggle.style.display = "hidden"
+}
+
+
+
+// event listeners  
+subButton.addEventListener('click', selectPlayers)
+
 
 // push that data itno the game state data
 
