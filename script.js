da3381c5d792f2728b7ea52bc47971decf90553b
Chris Perez <chrisnaperez@gmail.com>
2023-03-18 17:11:22 -0700

    added start game button function

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ const body = document.body;
 let singlePlayerButton = document.querySelector("#single-player-btn");
 let twoPlayerButton = document.querySelector("#two-players-btn");
 // const playerSelectButton = document.querySelector("#select-players")
-
+const startGameDiv = document.getElementById("start-game-button")
 // div dom manipulators
 const playerInputDom = document.querySelector("#player-input-dom")
 const playerDash = document.querySelector("#player-dash-dom")
 
 const gameDash = document.querySelector("game-dashboard");
 
-let PShiddenToggle = document.querySelector("#select-players")
 let playerOneInput = document.querySelector("#player-one-name");
 let playerTwoInput = document.querySelector("#player-two-name");
-let subButton = document.querySelector("#name-sub");
-let div = document.getElementsByClassName("#player-input")
-
 
 let startOverButton = document.getElementById('reset-game-btn');
-
+const startGameButton = document.getElementById('start-game-button')
 
 let hidePlayerOneBtns = document.getElementById("single-player-btn")
 let hidePlayerTwoBtns = document.getElementById("two-players-btn")
@@ -40,13 +36,12 @@ let gameData = {
 }
 
 
-
-
 // player button object with event listeners -- need to refactor to forget dry
 
 singlePlayerButton.addEventListener('click', () => {
     playerDash.classList.add("hidden")
     playerOneInput.classList.remove("hidden")
+    startGameButton.classList.remove("hidden")
     gameData.playerName[1] = 'Computer';
 })
 
@@ -56,25 +51,26 @@ twoPlayerButton.addEventListener('click', () => {
     playerDash.classList.add("hidden")
     playerOneInput.classList.remove("hidden")
     playerTwoInput.classList.remove("hidden")
-
+    startGameButton.classList.remove("hidden")
 })
 
 
 
-// player name submit 
+// player name submit button
 const startGame = () => {
     if (gameData.playerName[1] === 'Computer') {
-        gameData.playerName[0] = playerInput.value;
+        gameData.playerName[0] = playerOneInput.value;
         playerInputDom.classList.add("hidden")
-        playerInput.value = '';
+        playerOneInput.value = '';
     } else {
-
+        gameData.playerName[0] = playerOneInput.value;
+        gameData.playerName[1] = playerTwoInput.value;
+        playerInputDom.classList.add("hidden")
+        playerOneInput.value = '';
     }
-    // gameData.playerName[0] = playerInput.value;
-    // gameData.playerName[1] = playerInput.value;
 }
 
-
+// start over button 
 const startOver = () => {
     alert('Game has started over!')
     gameData = {
@@ -91,14 +87,11 @@ const startOver = () => {
     playerOneInput.classList.add("hidden")
     playerTwoInput.classList.add("hidden")
     playerInputDom.classList.remove("hidden")
+    startGameButton.classList.add("hidden")
 }
 
 
-// helper functions 
-
-
-
-
 // event listeners  
+startGameButton.addEventListener('click', startGame)
 startOverButton.addEventListener('click', startOver)
 
